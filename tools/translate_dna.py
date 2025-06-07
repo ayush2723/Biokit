bdd80202e53e006d8164d4e628254d70d8ebc727
ayush2723 <ayush1289sharma@gmail.com>
2025-06-08 00:11:15 +0530

    added full names of proteins

--- tools/translate_dna.py
+++ tools/translate_dna.py
@@ -25,7 +25,21 @@ def display_translation_view(dna_seq, rna_seq, protein_seq):
     codons_rna = chunk_string(rna_seq, 3)
     protein_chunks = list(protein_seq)
 
+    # Added dictionary for amino acid full names
+    amino_acid_names = {
+        'A': 'Alanine', 'R': 'Arginine', 'N': 'Asparagine', 'D': 'Aspartic Acid',
+        'C': 'Cysteine', 'Q': 'Glutamine', 'E': 'Glutamic Acid', 'G': 'Glycine',
+        'H': 'Histidine', 'I': 'Isoleucine', 'L': 'Leucine', 'K': 'Lysine',
+        'M': 'Methionine', 'F': 'Phenylalanine', 'P': 'Proline', 'S': 'Serine',
+        'T': 'Threonine', 'W': 'Tryptophan', 'Y': 'Tyrosine', 'V': 'Valine',
+        '*': 'Stop'
+    }
+
+    protein_names = [amino_acid_names.get(aa, '?') for aa in protein_chunks]
+
     st.markdown("### Translation Viewer")
     st.markdown("**DNA Codons:** `" + " ".join(codons_dna) + "`")
     st.markdown("**RNA Codons:** `" + " ".join(codons_rna) + "`")
     st.markdown("**Protein:** `" + " ".join(protein_chunks) + "`")
+    # Added full names display
+    st.markdown("**Full Names:** " + ", ".join(protein_names))
