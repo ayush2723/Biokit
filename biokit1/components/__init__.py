c1cd40fece3e31e70bbcca6bb8cbad63cb88f7b9
ayush2723 <ayush1289sharma@gmail.com>
2025-07-17 00:56:51 +0530

    Made changes to folder structure for more modularity

---
+++ biokit1/components/__init__.py
@@ -0,0 +1,5 @@
+# components/__init__.py
+# This file makes components a package.
+from .display import display_sequence
+from .input_box import dna_input_box
+from .plots import plot_nucleotide_composition
