492453aa445393b9d6e2915129986c3bfa8c08a0
ayush2723 <ayush1289sharma@gmail.com>
2025-07-18 01:08:18 +0530

    changed some details in input box section

--- biokit1/components/input_box.py
+++ biokit1/components/input_box.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-def dna_input_box(label="Enter DNA Sequence", height=100, max_chars=1000, placeholder="e.g. ATGCGTACGTTAGC"): #input box for DNA sequence
-    dna_input = st.text_area(label, height=height, max_chars=max_chars, placeholder=placeholder) #
+def dna_input_box(label="Enter DNA Sequence", key="dna_input_box", height=100, max_chars=1000, placeholder="e.g. ATGCGTACGTTAGC"): #input box for DNA sequence
+    dna_input = st.text_area(label,key=key, height=height, max_chars=max_chars, placeholder=placeholder) #
     seq_str = dna_input.replace(" ", "").replace("\n", "").upper() #replaces  whitespaces and new lines and convert to uppercase
     
     if dna_input and not set(seq_str).issubset({"A", "T", "G", "C"}): #validates sequence non empty strings evaluates to true
