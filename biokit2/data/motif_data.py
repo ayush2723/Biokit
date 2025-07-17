1b0846660a1463b12ec8639d560d94d5c01e4661
ayush2723 <ayush1289sharma@gmail.com>
2025-07-17 13:07:04 +0530

    modified directory structure

---
+++ biokit2/data/motif_data.py
@@ -0,0 +1,15 @@
+"""
+Predefined Common Motifs
+
+This file provides a dictionary of known biological motifs commonly used
+for promoter scanning or motif enrichment studies.
+"""
+
+COMMON_MOTIFS = {
+    "TATA box": "TATAAA",
+    "GC box": "GGGCGG",
+    "CAAT box": "CCAAT",
+    "BRE": "SSRCGCC",  # Simplified
+    "Inr": "YYANWYY",  # Simplified degenerate format
+    "Poly-A signal": "AATAAA"
+}
