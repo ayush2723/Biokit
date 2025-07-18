8756ea36dfa7cda610c16af18f42446bc102e6d8
ayush2723 <ayush1289sharma@gmail.com>
2025-07-19 00:55:09 +0530

    standarized data for restriction enzymes

---
+++ biokit2/data/restriction_enzyme.py
@@ -0,0 +1,13 @@
+restriction_enzymes = {
+    "EcoRI": "GAATTC",
+    "BamHI": "GGATCC",
+    "HindIII": "AAGCTT",
+    "NotI": "GCGGCCGC",
+    "TaqI": "TCGA",
+    "HaeIII": "GGCC",
+    "PstI": "CTGCAG",
+    "XhoI": "CTCGAG",
+    "SmaI": "CCCGGG",
+    "SalI": "GTCGAC"
+}
+#standard data for enzymes restriction sites
